693b03ca6f1ac3e381a583b604d1b4dfd2b6150d
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-02-21 01:26:36 -0500

    added javascript code for the questions

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+let quizData = [
+    {
+    question: "What does HTML stand for?",
+    a: "High Times Miller Light",
+    b: "Hello World",
+    c: "Hyper Text Markup Language",
+    d: "None of the above",
+    }
+    {
+    question: "What year was HTML released?",
+    a: "2002",
+    b: "1998",
+    c: "1985",
+    d: "1996",
+    },
+
+    {
+    question: 'What does CSS stand for?',
+    a: ' Center Standard Style',
+    b: ' Cascading Style Sheets',
+    c: 'Computer Syntax Standard',
+    d: 'All of the above'
+    },
+    {
+    question: " What year was javascript released?",
+    a:"1998",
+    b:"1996",
+    c:"1999",
+    d:"None of the above"
+    }
+
+];
+
